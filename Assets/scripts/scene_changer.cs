b724778b3ed6da35a3d5ec86dbb15cdc2a0c5663
Pain-n <58704613+Pain-n@users.noreply.github.com>
2019-12-12 19:54:44 +0300

    Initial commit

---
+++ Assets/scripts/scene_changer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_changer : MonoBehaviour
+{
+    public void Load_scene(int q)
+    {
+        SceneManager.LoadScene(q-1);
+    }
+}
